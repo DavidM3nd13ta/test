83b875d0935faddd865f178abfc2639c52df6eb9
DavidM3nd13ta <145582608+DavidM3nd13ta@users.noreply.github.com>
2023-11-08 20:49:15 -0500

    Add files via upload

---
+++ demo/src/main/java/com/javacrud/demo/JavaCrudApplication.java
@@ -0,0 +1,26 @@
+package com.javacrud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudApplication.class, args);
+	}
+
+@GetMapping("/")
+public String init() {
+
+Como desplegar una aplicación de java en Azure 8
+
+return String.format("Welcome to Java CRUD Application!");
+}
+
+@GetMapping("/hello")
+public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+return String.format("Hello %s!", name);
+}
+
+}
